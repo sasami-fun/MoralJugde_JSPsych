fbb097c1f9c03f42686234aea41640552a88f694
@sasami-fun <hi.sasamori.n09@gmail.com>
2022-01-17 16:34:01 +0900

    firebaseSDK初期化

---
+++ index.js
@@ -0,0 +1,14 @@
+if(!firebase.apps.length){
+    var firebaseConfig = {
+      apiKey: "AIzaSyC09cTB2qw88WvysDUid5O6pQCAepL1o1A",
+      authDomain: "judgement-9d4dc.firebaseapp.com",
+      databaseURL: "https://judgement-9d4dc-default-rtdb.asia-southeast1.firebasedatabase.app",
+      projectId: "judgement-9d4dc",
+      storageBucket: "judgement-9d4dc.appspot.com",
+      messagingSenderId: "266523739421",
+      appId: "1:266523739421:web:0e02b7ca4dc04123524df4"
+    };
+    firebase.initializeApp(firebaseConfig);
+}
+
+console.log("1");
