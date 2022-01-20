e09a6450c1e7ee9eab7beae84fce4e151aba607b
@sasami-fun <hi.sasamori.n09@gmail.com>
2022-01-20 17:40:48 +0900

    シナリオ誤字訂正、教示文調整、サーバーver

--- intro.js
+++ intro.js
@@ -6,7 +6,8 @@ var welcome1 = {
   '<br>Wifi接続や電源が途中で切れないようご準備ください。'+
   '<br>実験ではキーボードを使用します。</p>'+
   '<p>よろしければキーボードの[↓]を押してください</p>',
-  choices:['ArrowDown']
+  choices:['ArrowDown'],
+  data: {test_part: 'welcome1'}
 };
 var welcome2 = {
   type: 'html-keyboard-response',
@@ -17,7 +18,8 @@ var welcome2 = {
   '<br>実験中は外部と連絡を取ったり検索したりしないでください。'+
   '<br>気が散らないようにPC等の通知などは切っておいてください。</p>'+
   '<p>よろしければキーボードの[↓]を押してください</p>',
-  choices:['ArrowDown']
+  choices:['ArrowDown'],
+  data: {test_part: 'welcome2'}
 };
 var fullscreen = {
   type: 'fullscreen',
@@ -26,7 +28,8 @@ var fullscreen = {
   '<br>実験終了まで全画面モードを解除しないでください。'+
   '<br>実験を途中で辞退したい場合は[Esc]を押してウィンドウを閉じてください。</p>',
   button_label: "全画面表示に切り替え",
-  fullscreen_mode: true
+  fullscreen_mode: true,
+  data: {test_part: 'fullscreen'}
 };
 var intro_operation0 = {
   type: 'html-keyboard-response',
@@ -34,7 +37,8 @@ var intro_operation0 = {
   '<p>部屋はできるだけ明るくしてください。'+
   '<br>画面と目の距離は40cm以上離して一定にして姿勢を正してください。</p>'+
   '<p>よろしければ[↓]を押してください。</p>',
-  choices:['ArrowDown']
+  choices:['ArrowDown'],
+  data: {test_part: 'intro_operation0'}
 };
 var intro_operation1 = {
   type: 'html-keyboard-response',
@@ -45,7 +49,8 @@ var intro_operation1 = {
   '<br>シナリオが表示される前に、注視点として「＋」が短い間だけ表示されます。'+
   '<br>シナリオの最後の文章は質問です。</p>'+
   '<p>内容を理解したら[↓]を押してください。</p>',
-  choices:['ArrowDown']
+  choices:['ArrowDown'],
+  data: {test_part: 'intro_operation1'}
 };
 var intro_operation2 = {
   type: 'html-keyboard-response',
@@ -56,51 +61,48 @@ var intro_operation2 = {
   '<br>必ずどちらか選択して答えてください。'+
   '<br>時間制限はありませんが、できるだけすばやく正確に答えてください。</p>'+
   '<p>内容を理解したら[↓]を押してください。</p>',
-  choices:['ArrowDown']
+  choices:['ArrowDown'],
+  data: {test_part: 'intro_operation2'}
 };
 var intro_operation3 = {
   type: 'html-keyboard-response',
   stimulus: '<p class="top0">姿勢を正してください。'+
-  '<br>キーボードの[←]と[→]に左右の人差し指をそれぞれ軽く置いて構えてください。'+
+  '<br>キーボードの[←]と[→]に両手の人差し指をそれぞれ軽く置いて構えてください。'+
+  '<br>キーは長押しせず、軽く押したらすぐ離すようにしてください。'+
   '<br>シナリオは自分のペースで読み進めてください。</p>'+
   '<p>練習パートを始めます。'+
   '<br>よろしければ[↓]を押してください。</p>',
-  choices:['ArrowDown']
+  choices:['ArrowDown'],
+  data: {test_part: 'intro_operation3'}
 };
 var instruction_main0 = {
   type: 'html-keyboard-response',
-  stimulus: '<p>それでは本番パートです。</p>'+
+  stimulus: '<p class="top0">練習パートは終了です。</p>'+
   '<p>***操作確認***'+
-  '<br>キーボードの矢印キー[←][↓][→]を使用します。'+
-  '<br>それぞれの文章は一度しか表示されません。'+
-  '<br>シナリオの初めの文が表示される前に、注視点として「＋」が短い間だけ表示されます。'+
-  '<br>文章を理解したら[↓]を押すと次の文が表示されます。'+
-  '<br>シナリオの最後の文章は質問です。質問には'+
-  '<br>「はい」の場合は[←]、'+
-  '<br>「いいえ」の場合は[→]'+
-  '<br>を押して答えてください。'+
-  '<br>必ずどちらか選択して答えてください。'+
-  '<br>時間制限はありませんが、できるだけすばやく正確に答えてください。'+
-  '<br>姿勢を正してください。'+
-  '<br>キーボードの[←]と[→]に左右の人差し指をそれぞれ軽く置いて構えてください。'+
-  '<br>シナリオは自分のペースで読み進めてください。</p>'+
+  '<br>次へ：[↓]　　はい：[←]　　いいえ：[→]'+
+  '<br>質問には必ずどちらか選択して答えてください。'+
+  '<br>本番パートでは矢印キーの案内は表示されません。</p>'+
   '<p>よろしければ[↓]を押してください。</p>',
   choices:['ArrowDown'],
+  data: {test_part: 'instruction_main0'}
 }
 var instruction_main1 = {
   type: 'html-keyboard-response',
   stimulus: '<p class="top0">それでは本番パートです。</p>'+
-  '<br>姿勢を正してください。'+
-  '<br>キーボードの[←]と[→]に左右の人差し指をそれぞれ軽く置いて構えてください。'+
+  '<p>時間制限はありませんが、できるだけすばやく正確に答えてください。'+
+  '<br>キーは長押しせず、軽く押したらすぐ離すようにしてください。'+
+  '<br>姿勢を正し、キーボードの[←]と[→]に両手の人差し指をそれぞれ軽く置いて構えてください。'+
   '<p>[↓]を押すと開始します。</p>',
   choices:['ArrowDown'],
+  data: {test_part: 'instruction_main1'}
 }
 var end_message = {
   type: 'html-keyboard-response',
-  stimulus: '<p class = "top0>これで実験を終了します。</p>'+
+  stimulus: '<p class = "top0">これで実験を終了します。</p>'+
   '<p>研究にご協力頂きありがとうございました。'+
   '<br>実験データが確認でき次第、メールにて謝金支払いをご案内します。</p>'+
   '<p>[スペース]を押して実験サイトを閉じます。'+
-  '<br>[スペース]を押すと画面が暗転しますので、[Ecs]を押して全画面モードを解除した後、ウィンドウを閉じてください。</p>',
+  '<br>[スペース]を押した後、画面が暗転しますので[Ecs]を押して全画面モードを解除した後、ウィンドウを閉じてください。</p>',
   choices:[' '],
+  data: {test_part: 'end_message'}
  }
